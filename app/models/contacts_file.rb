04551203d93844ddde0efa5947dbb518e370786e
Andrés Felipe Mejía <andres.mejia@symplifica.com>
2022-01-10 21:47:50 -0500

    adds contacts_file model with tests

---
+++ app/models/contacts_file.rb
@@ -0,0 +1,21 @@
+class ContactsFile < ApplicationRecord
+  validates :name, presence: true
+  belongs_to :user
+
+  has_one_attached :csv_file
+
+  enum status: { pending: 'ON HOLD',
+                  processing: 'PROCESSING',
+                  failed: 'FAILED',
+                  finished: 'TERMINATED'
+  }
+
+  before_create :init_status
+
+  HEADER_COLUMNS = %i[name birthdate phone_number address email cc_mask].freeze
+
+  private
+  def init_status
+    self.status = ContactsFile.statuses[:pending]
+  end
+end
