bceb4af36f39ee9f5c54628b3fafd2f52524d789
Andrés Felipe Mejía <andres.mejia@symplifica.com>
2022-01-10 19:39:48 -0500

    adds contacts list view and route

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -6,7 +6,7 @@ class ContactTest < ActiveSupport::TestCase
   # end
   test "Name should be valid" do
     @user = User.first
-    @contact = Contact.new(name: '%', birthdate: Date.parse('1987-05-19'),
+    @contact = Contact.new(name: '%', birthdate: Date.new(1987, 5, 19),
                           phone_number: '(+57) 310 477 07 18', address: 'Cra 11 # 95 - 37',
                           cc_mask: '4111111111111111', email: 'test@email.com', user: @user)
     assert_not @contact.valid?
@@ -14,7 +14,7 @@ class ContactTest < ActiveSupport::TestCase
 
   test "Phone number should be valid" do
     @user = User.first
-    @contact = Contact.new(name: 'Valid name', birthdate: Date.parse('1987-05-19'),
+    @contact = Contact.new(name: 'Valid name', birthdate: Date.new(1987, 5, 19),
                           phone_number: '(+57)3104770718', address: 'Cra 11 # 95 - 37',
                           cc_mask: '4111111111111111', email: 'test@email.com', user: @user)
     assert_not @contact.valid?
@@ -22,7 +22,7 @@ class ContactTest < ActiveSupport::TestCase
 
   test "Address must not be blank" do
     @user = User.first
-    @contact = Contact.new(name: 'Valid name', birthdate: Date.parse('1987-05-19'),
+    @contact = Contact.new(name: 'Valid name', birthdate: Date.new(1987, 5, 19),
                           phone_number: '(+57) 310 477 07 18', address: ' ',
                           cc_mask: '4111111111111111', email: 'test@email.com', user: @user)
     assert_not @contact.valid?
@@ -30,7 +30,7 @@ class ContactTest < ActiveSupport::TestCase
 
   test "Credit card info must be valid" do
     @user = User.first
-    @contact = Contact.new(name: 'Valid name', birthdate: Date.parse('1987-05-19'),
+    @contact = Contact.new(name: 'Valid name', birthdate: Date.new(1987, 5, 19),
                           phone_number: '(+57) 310 477 07 18', address: 'Cra 11 # 95 - 37',
                           cc_mask: '251111111', email: 'test@email.com', user: @user)
     assert_not @contact.valid?
@@ -38,7 +38,7 @@ class ContactTest < ActiveSupport::TestCase
 
   test "Email must be valid" do
     @user = User.first
-    @contact = Contact.new(name: 'Valid name', birthdate: Date.parse('1987-05-19'),
+    @contact = Contact.new(name: 'Valid name', birthdate: Date.new(1987, 5, 19),
                           phone_number: '(+57) 310 477 07 18', address: 'Cra 11 # 95 - 37',
                           cc_mask: '4111111111111111', email: 'invalid email', user: @user)
     assert_not @contact.valid?
@@ -46,10 +46,10 @@ class ContactTest < ActiveSupport::TestCase
 
   test "Contact email must be unique for user" do
     @user = User.first
-    temp_contact =Contact.create(name: 'Valid name', birthdate: Date.parse('1987-05-19'),
+    temp_contact =Contact.create(name: 'Valid name', birthdate: Date.new(1987, 5, 19),
                               phone_number: '(+57) 310 477 07 18', address: 'Cra 11 # 95 - 37',
                               cc_mask: '4111111111111111', email: 'test@email.com', user: @user)
-    @contact = Contact.new(name: 'Valid name', birthdate: Date.parse('1987-05-19'),
+    @contact = Contact.new(name: 'Valid name', birthdate: Date.new(1987, 5, 19),
                           phone_number: '(+57) 310 477 07 18', address: 'Cra 11 # 95 - 37',
                           cc_mask: '4111111111111111', email: 'test@email.com', user: @user)
     assert_not @contact.valid?
@@ -57,7 +57,7 @@ class ContactTest < ActiveSupport::TestCase
 
   test "Contact is valid" do
     @user = User.first
-    @contact = Contact.new(name: 'Valid name', birthdate: Date.parse('1987-05-19'),
+    @contact = Contact.new(name: 'Valid name', birthdate: Date.new(1987, 5, 19),
                           phone_number: '(+57) 310 477 07 18', address: 'Cra 11 # 95 - 37',
                           cc_mask: '4111111111111111', email: 'test@email.com', user: @user)
     assert @contact.valid?
