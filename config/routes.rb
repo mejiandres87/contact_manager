3accd589c9b0dd5e1f3447184adb99472d5e5d40
Andrés Felipe Mejía <andres.mejia@symplifica.com>
2022-01-11 17:38:12 -0500

    adds asyncronous importing adds sidekiq monitoring endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
+  
   resources :contacts_files, only: [ :show, :create, :index] do 
     member do 
       post '/import', to: 'contacts_files#import_contacts_from_file'
